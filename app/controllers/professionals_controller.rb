5e9344d6742d20c3d56163e63a26aed6554f3847
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2025-04-17 14:25:04 +0100

    Defines professional model

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,4 +1,48 @@
 class ProfessionalsController < ApplicationController
+  before_action :set_professional, only: %i[ show edit update ] 
+
   def index
+    @professionals = Professional.all
   end
+
+  def show
+  end
+
+  def new
+    @professional = Professional.new
+  end
+
+  def create
+    @professional = Professional.new
+    if @professional.save
+      redirect_to @professional
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @professional.update(professional_params)
+      redirect_to @profesional
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @professional.destroy
+    redirect_to professionals_path
+  end
+
+  private
+    def set_professional
+      @professional = Professional.find(params[:id])
+    end
+
+    def professional_params
+      params.expect(professional: [:name, :order_number])
+    end
 end
--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,2 +1,11 @@
 class Professional < ApplicationRecord
+  after_initialize :set_default_status, if: :new_record?
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :order_number, presence: true, numericality: { only_integer: true }
+
+  private
+    def set_default_status
+      self[:status] ||= :pending
+    end
 end
