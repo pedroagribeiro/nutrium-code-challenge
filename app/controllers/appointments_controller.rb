2b1fe7c82a5e9c43ea722b16cedb99451deb17fa
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2025-04-18 02:24:14 +0100

    Adds way of querying pending appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,13 @@
 class AppointmentsController < ApplicationController
   def index
+    professional_id = params[:professional_id]
+    status = params[:status]
+
     appointments = Appointment.includes(:professional, :service).all
+
+    appointments = appointments.where(professional_id: professional_id) if professional_id.present?
+    appointments = appointments.where(status: status) if status.present?
+
     render inertia: 'Appointments/Index', props: {
       appointments: appointments.as_json(include: [:professional, :service])
     }
